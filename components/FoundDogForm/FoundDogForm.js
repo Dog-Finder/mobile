8043c3c8f30e8db755d31d1c8d7be0c14536c66a
Yoav Navon <yanavon@uc.cl>
2020-07-23 19:48:53 -0400

    FoundDogForm with hooks

--- components/FoundDogForm/FoundDogForm.js
+++ components/FoundDogForm/FoundDogForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, View, TouchableOpacity } from 'react-native'
 import PropTypes from 'prop-types'
 import * as Location from 'expo-location'
@@ -7,59 +7,46 @@ import DateTimeInput from 'components/inputs/DateTimeInput'
 import { SexInput } from 'components/inputs/SexInput'
 import MapInput from 'components/inputs/MapInput'
 
-export default class FoundDogForm extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: '',
-      sex: '',
-      comentary: '',
-      date: new Date(),
-      marker: { latitude: 51.5078788, longitude: -0.0877321 },
-      street: '',
-      city: '',
-      country: '',
-      loading: false,
-    }
-    this.onDragEnd = this.onDragEnd.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
+const FoundDogForm = ({ onSubmitHandler, pressPicture, imagePath }) => {
+  const [name, setName] = useState('')
+  const [sex, setSex] = useState('')
+  const [comentary, setComentary] = useState('')
+  const [date, setDate] = useState(new Date())
+  const [marker, setMarker] = useState({
+    latitude: 51.5078788,
+    longitude: -0.0877321,
+  })
+  const [street, setStreet] = useState('')
+  const [city, setCity] = useState('')
+  const [country, setCountry] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const setAddress = async marker => {
+    const address = await Location.reverseGeocodeAsync(marker)
+    const { street, city, country } = address[0]
+    setStreet(street)
+    setCity(city)
+    setCountry(country)
   }
 
-  async componentDidMount() {
-    const { status } = await Location.requestPermissionsAsync()
-    if (status === 'granted') {
-      const address = await Location.reverseGeocodeAsync(this.state.marker)
-      this.setState({
-        street: address[0].street,
-        city: address[0].city,
-        country: address[0].country,
-      })
+  useEffect(() => {
+    async function initialLocation() {
+      const { status } = await Location.requestPermissionsAsync()
+      if (status === 'granted') {
+        setAddress(marker)
+      }
     }
-  }
+    initialLocation()
+  }, [])
 
-  async onDragEnd(event) {
+  const onDragEnd = async event => {
     const coordinate = event.nativeEvent.coordinate
-    this.setState({ marker: coordinate })
-    const address = await Location.reverseGeocodeAsync(coordinate)
-    this.setState({
-      street: address[0].street,
-      city: address[0].city,
-      country: address[0].country,
-    })
+    setMarker(coordinate)
+    setAddress(coordinate)
   }
 
-  onSubmit() {
-    this.setState({ loading: true })
-    const {
-      name,
-      sex,
-      comentary,
-      date,
-      marker,
-      street,
-      city,
-      country,
-    } = this.state
+  const onSubmit = () => {
+    setLoading(true)
     const data = {
       name,
       sex,
@@ -68,71 +55,54 @@ export default class FoundDogForm extends Component {
       marker,
       address: { street, city, country },
     }
-
-    this.props.onSubmitHandler(1234, data)
+    onSubmitHandler(1234, data)
   }
 
-  render() {
-    return (
-      <View>
-        <Card title="Información General">
-          <TouchableOpacity onPress={this.props.pressPicture}>
-            <Image
-              style={styles.image}
-              resizeMode="cover"
-              source={{ uri: this.props.imagePath }}
-            />
-          </TouchableOpacity>
-          <Input
-            placeholder="Nombre"
-            onChangeText={name => {
-              this.setState({ name })
-            }}
-            value={this.state.name}
+  return (
+    <View>
+      <Card title="Información General">
+        <TouchableOpacity onPress={pressPicture}>
+          <Image
+            style={styles.image}
+            resizeMode="cover"
+            source={{ uri: imagePath }}
           />
-          <DateTimeInput
-            placeholder="¿Cuándo lo encontraste?"
-            isVisible={false}
-            onConfirm={date => {
-              this.setState({ date })
-            }}
-            onCancel={() => {}}
-          />
-          <SexInput
-            onValueChange={sex => {
-              this.setState({ sex })
-            }}
-          />
-          <Input
-            placeholder="Descripción"
-            onChangeText={comentary => {
-              this.setState({ comentary })
-            }}
-            value={this.state.comentary}
-          ></Input>
-        </Card>
-        <Card title="Ubicación">
-          <MapInput
-            marker={this.state.marker}
-            street={this.state.street}
-            city={this.state.city}
-            country={this.state.country}
-            onDragEnd={this.onDragEnd}
-            onChangeStreet={street => this.setState({ street })}
-            onChangeCity={city => this.setState({ city })}
-            onChangeCountry={country => this.setState({ country })}
-          />
-        </Card>
-        <Card>
-          <Button
-            title="Submit"
-            onPress={this.onSubmit}
-            loading={this.state.loading}
-          ></Button>
-        </Card>
-      </View>
-    )
-  }
+        </TouchableOpacity>
+        <Input
+          placeholder="Nombre"
+          onChangeText={name => setName(name)}
+          value={name}
+        />
+        <DateTimeInput
+          placeholder="¿Cuándo lo encontraste?"
+          isVisible={false}
+          onConfirm={date => setDate(date)}
+          onCancel={() => {}}
+        />
+        <SexInput onValueChange={sex => setSex(sex)} />
+        <Input
+          placeholder="Descripción"
+          onChangeText={comentary => setComentary(comentary)}
+          value={comentary}
+        ></Input>
+      </Card>
+      <Card title="Ubicación">
+        <MapInput
+          marker={marker}
+          street={street}
+          city={city}
+          country={country}
+          onDragEnd={onDragEnd}
+          onChangeStreet={street => setStreet(street)}
+          onChangeCity={setCity}
+          onChangeCountry={setCountry}
+        />
+      </Card>
+      <Card>
+        <Button title="Submit" onPress={onSubmit} loading={loading}></Button>
+      </Card>
+    </View>
+  )
 }
 
 FoundDogForm.propTypes = {
@@ -148,3 +118,5 @@ const styles = StyleSheet.create({
     width: null,
   },
 })
+
+export default FoundDogForm
