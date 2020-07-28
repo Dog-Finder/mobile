06db090eba1b581507c8501fd6a12277447bb73f
Yoav Navon <yanavon@uc.cl>
2020-07-27 23:05:11 -0400

    List views to functional

--- screens/LostDogListScreen.js
+++ screens/LostDogListScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { SafeAreaView, ScrollView, StyleSheet } from 'react-native'
 import PropTypes from 'prop-types'
 import Constants from 'expo-constants'
@@ -6,46 +6,44 @@ import Constants from 'expo-constants'
 import LostDogItem from '../components/LostDog/LostDogItem'
 import { getLostDogList } from '../api'
 
-class LostDogListScreen extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { lostDogList: [] }
-  }
-  getCurrentLocation = async () => {
+const LostDogListScreen = ({ navigation }) => {
+  const [lostDogList, setLostDogList] = useState([])
+  const [latitude, setLatitude] = useState(null)
+  const [longitude, setLongitude] = useState(null)
+  const getCurrentLocation = async () => {
     navigator.geolocation.getCurrentPosition(async position => {
-      this.setState({
-        latitude: parseFloat(position.coords.latitude),
-        longitude: parseFloat(position.coords.longitude),
-      })
+      setLatitude(parseFloat(position.coords.latitude))
+      setLongitude(parseFloat(position.coords.longitude))
     })
   }
-  async componentDidMount() {
-    const { data } = await getLostDogList(1234)
-    this.setState({ lostDogList: data.resource })
-    this.getCurrentLocation()
-  }
+  useEffect(() => {
+    // eslint-disable-next-line prettier/prettier
+    (async () => {
+      const { data } = await getLostDogList(1234)
+      setLostDogList(data.resource)
+      getCurrentLocation()
+    })()
+  }, [])
 
-  render() {
-    const lostDogList = this.state.lostDogList.map((lostDog, i) => {
-      return (
-        <LostDogItem
-          key={i}
-          dog={lostDog}
-          navigator={this.props.navigation}
-          userCoordinates={{
-            latitude: this.state.latitude,
-            longitude: this.state.longitude,
-          }}
-        ></LostDogItem>
-      )
-    })
-
+  const lostDogItems = lostDogList.map((lostDog, i) => {
     return (
-      <SafeAreaView styles={styles.container}>
-        <ScrollView styles={styles.scrollView}>{lostDogList}</ScrollView>
-      </SafeAreaView>
+      <LostDogItem
+        key={i}
+        dog={lostDog}
+        navigator={navigation}
+        userCoordinates={{
+          latitude,
+          longitude,
+        }}
+      ></LostDogItem>
     )
-  }
+  })
+
+  return (
+    <SafeAreaView styles={styles.container}>
+      <ScrollView styles={styles.scrollView}>{lostDogItems}</ScrollView>
+    </SafeAreaView>
+  )
 }
 
 LostDogListScreen.propTypes = {
