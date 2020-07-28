727b4389ab1cb091d661c2e41651730d14b545d2
Ian Butelmann <ibutelmann@uc.cl>
2020-07-27 22:44:20 -0400

    LostDogItem, stack, screen and showlostdoginfoscreen done, copying founddog items and adding dog name at top of showlostdoginfoscreen (#9)

---
+++ components/LostDog/LostDogItem.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, TouchableOpacity } from 'react-native'
+import PropTypes from 'prop-types'
+import { Card } from 'react-native-elements'
+import getDistance from 'geolib/es/getDistance'
+
+export default class LostDogItem extends Component {
+  constructor(props) {
+    super(props)
+  }
+  static propTypes = {
+    dog: PropTypes.object.isRequired,
+    navigator: PropTypes.object.isRequired,
+    userCoordinates: PropTypes.object.isRequired,
+  }
+
+  render() {
+    const { imageLinks, date, sex, marker } = this.props.dog //props: imageLinks, date, comentary, address, sex, marker, name
+    const parsedDate = new Date(date)
+    const navigator = this.props.navigator
+    let distance
+    try {
+      distance = getDistance(marker, this.props.userCoordinates) / 1000
+    } catch (error) {
+      distance = 'error'
+    }
+    return (
+      <TouchableOpacity
+        delayPressIn={30}
+        onPress={() =>
+          navigator.push('ShowLostDogInfo', {
+            dogInfo: this.props.dog,
+          })
+        }
+      >
+        <Card image={{ uri: imageLinks }} imageStyle={styles.image}>
+          <View>
+            <Text>Fecha Aviso: {parsedDate.toDateString()}</Text>
+            <Text>Sexo: {sex}</Text>
+            <Text>
+              Distancia a ubicación actual:{' '}
+              {Math.round((distance + Number.EPSILON) * 10) / 10} km.
+            </Text>
+          </View>
+        </Card>
+      </TouchableOpacity>
+    )
+  }
+}
+//{Math.round((distance + Number.EPSILON) * 10) / 10} is for keeping only one decimal place for distance
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    height: 300,
+    width: null,
+  },
+})
