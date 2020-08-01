16371049c20919946f99b020039dc9eb17870aaf
Ian Butelmann <ibutelmann@uc.cl>
2020-07-31 22:16:03 -0400

    fixing map on android and some other things (#13)
    * marker on map now moves correctly on android, region and marker starter point is current location, delete name from lostdogfom, adding number to props
    
    * trying to fix conflicts

--- components/inputs/MapInput.js
+++ components/inputs/MapInput.js
@@ -1,42 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View } from 'react-native'
 import { Input } from 'react-native-elements'
 import MapView, { Marker } from 'react-native-maps'
+import * as Location from 'expo-location'
 import PropTypes from 'prop-types'
 
 const MapInput = props => {
+  useEffect(() => {
+    async function initialLocation() {
+      const { status } = await Location.requestPermissionsAsync()
+      if (status === 'granted') {
+        props.getCurrentLocation(this.mapView)
+      }
+    }
+    initialLocation()
+  }, [])
   return (
     <View style={{ height: 300 }}>
       <MapView
         style={{ flex: 1 }}
-        initialRegion={{
-          latitude: props.marker.latitude,
-          longitude: props.marker.longitude,
-          latitudeDelta: 0.009,
-          longitudeDelta: 0.009,
-        }}
+        initialregion={props.initialRegion}
+        onPress={props.onDragEnd}
+        ref={ref => (this.mapView = ref)}
       >
         <Marker
           draggable
           coordinate={props.marker}
-          title={'Mover el pin al punto donde encontraste al perro.'}
+          title={'Mover al punto donde encontraste al perro.'}
           onDragEnd={props.onDragEnd}
         />
       </MapView>
       <Input
         onChangeText={props.onChangeStreet}
         placeholder="Calle"
-        value={props.street}
+        value={props.street + ' ' + props.number}
+        disabled={true}
       ></Input>
       <Input
         onChangeText={props.onChangeCity}
         placeholder="Ciudad"
         value={props.city}
+        disabled={true}
       ></Input>
       <Input
         onChangeText={props.onChangeCountry}
         placeholder="Pais"
         value={props.country}
+        disabled={true}
       ></Input>
     </View>
   )
@@ -44,13 +54,16 @@ const MapInput = props => {
 
 MapInput.propTypes = {
   marker: PropTypes.object.isRequired,
-  street: PropTypes.string.isRequired,
-  city: PropTypes.string.isRequired,
-  country: PropTypes.string.isRequired,
+  number: PropTypes.string,
+  street: PropTypes.string,
+  city: PropTypes.string,
+  country: PropTypes.string,
+  initialRegion: PropTypes.object.isRequired,
   onDragEnd: PropTypes.func.isRequired,
   onChangeStreet: PropTypes.func.isRequired,
   onChangeCity: PropTypes.func.isRequired,
   onChangeCountry: PropTypes.func.isRequired,
+  getCurrentLocation: PropTypes.func.isRequired,
 }
 
 export default MapInput
